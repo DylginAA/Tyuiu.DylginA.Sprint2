7dace2787abad099ffa69b27b37728d6e5193f9f
Anton Dylgin <Tyuiu.DylginA@outlook.com>
2024-10-14 21:02:34 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.DylginA.Sprint2.Task0.V18.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.DylginA.Sprint2.Task0.V18.Lib
+{
+    public class DataService : ISprint2Task0V18
+    {
+        //(==, !=, <, >, <=, >=, последовательность операций не должна нарушаться) и арифметических выражений, которая вернет логическую
+        //последовательность(массив): (False, True, True, True, True, False), при x = 5105, y = 475
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            
+           bool[] res = new bool[6];
+            res[0] = x == y;
+            res[1] = x != y;
+            res[2] = x<y+4631;
+            res[3] = x>y;
+            res[4] = x<=y+4630;
+            res[5] = x-5000>=y;
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.DylginA.Sprint2.Task0.V18/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.DylginA.Sprint2.Task0.V18.Lib;
+namespace Tyuiu.DylginA.Sprint2.Task0.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #0 | Выполнил: Дылгин А. | ИБКСБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#");
+            Console.WriteLine("* Задание #0");
+            Console.WriteLine("* Вариант #18");
+            Console.WriteLine("* Выполнил: Дылгин А. | ИБКСБ-24-1");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:Написать программу из операций сравнений (==, !=, <, >, <=, >=, последовательность операций не должна нарушаться) " +
+                "и арифметических выражений, которая вернет логическую последовательность(массив): (False, True, True, True, True, False), " +
+                "при x = 5105, y = 475");
+            Console.WriteLine("");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine();
+            Console.WriteLine("x = 5105");
+            Console.WriteLine("x = 475");
+            int x  = 5105;
+            int y = 475;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.GetCompareOperations(x,y));
+        }
+    }
+}
